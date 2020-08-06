49e793769e41cdf73daaf6954bb5d054ffb390c8
Diego Mombelli <48128187+DiegoMombe@users.noreply.github.com>
2020-08-06 13:01:00 +0200

    Completed website!

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-var nodemailer = require('nodemailer');
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
-var transport = nodemailer.createTransport({
-    host: "mail.righi-srl.it",
-    port: 587,
-    auth: {
-        user: "righi-srl.it",
-        pass: "Ri2020$Mom",
-    }
-});
-
-var mailOptions = {
-    from: 'info@righi-srl.it',
-    to: 'info@righi-srl.it',
-    subject: 'prova, sono Diego',
-    text: 'Hey there, it’s our first message sent with Nodemailer ;) ',
-    html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer',
-};
-
-transport.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        return console.log(error);
-    }
-    console.log('Message sent: %s', info.messageId);
-});
